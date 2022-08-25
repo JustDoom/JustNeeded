8d49aedaef6be25d8b203230fb324f0865b9f736
JustDoom <justdoomdev@gmail.com>
2022-08-25 10:54:16 +1000

    Shears now work and re-added the fuels

--- common/src/main/java/com/imjustdoom/justneeded/JustNeeded.java
+++ common/src/main/java/com/imjustdoom/justneeded/JustNeeded.java
@@ -4,22 +4,41 @@ import com.google.common.base.Suppliers;
 import com.imjustdoom.justneeded.block.BlockInit;
 import com.imjustdoom.justneeded.item.ItemInit;
 import dev.architectury.registry.CreativeTabRegistry;
+import dev.architectury.registry.fuel.FuelRegistry;
 import dev.architectury.registry.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 import java.util.function.Supplier;
 
 public class JustNeeded {
     public static final String MOD_ID = "justneeded";
-    // We can use this if we don't want to use DeferredRegister
-    public static final Supplier<Registries> REGISTRIES = Suppliers.memoize(() -> Registries.get(MOD_ID));
 
     public static final CreativeModeTab EXAMPLE_TAB = CreativeTabRegistry.create(new ResourceLocation(MOD_ID, "justneeded_tab"), () -> new ItemStack(ItemInit.COOKED_EGG.get()));
 
     public static void init() {
         BlockInit.init();
         ItemInit.init();
+
+        FuelRegistry.register(80, Items.PAPER);
+        FuelRegistry.register(255, Items.BOOK);
+        FuelRegistry.register(255, Items.WRITTEN_BOOK);
+        FuelRegistry.register(255, Items.WRITABLE_BOOK);
+        FuelRegistry.register(80, Items.MAP);
+        FuelRegistry.register(80, Items.FILLED_MAP);
+        FuelRegistry.register(3745, Items.HAY_BLOCK);
+        FuelRegistry.register(85, Items.ACACIA_LEAVES);
+        FuelRegistry.register(85, Items.BIRCH_LEAVES);
+        FuelRegistry.register(85, Items.DARK_OAK_LEAVES);
+        FuelRegistry.register(85, Items.JUNGLE_LEAVES);
+        FuelRegistry.register(85, Items.OAK_LEAVES);
+        FuelRegistry.register(85, Items.SPRUCE_LEAVES);
+        FuelRegistry.register(90, Items.GRASS);
+        FuelRegistry.register(90, Items.FERN);
+        FuelRegistry.register(90, Items.LARGE_FERN);
+        FuelRegistry.register(90, Items.TALL_GRASS);
+        FuelRegistry.register(90, Items.VINE);
     }
 }
