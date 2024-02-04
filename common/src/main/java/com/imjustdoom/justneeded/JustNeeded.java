31fb8a3254663200b4e0a754a269668f4e7778b4
JustDoom <61824552+JustDoom@users.noreply.github.com>
2024-02-04 18:38:17 +1000

    Lightening makes sand into glass

--- common/src/main/java/com/imjustdoom/justneeded/JustNeeded.java
+++ common/src/main/java/com/imjustdoom/justneeded/JustNeeded.java
@@ -2,18 +2,27 @@ package com.imjustdoom.justneeded;
 
 import com.imjustdoom.justneeded.block.BlockInit;
 import com.imjustdoom.justneeded.item.ItemInit;
+import dev.architectury.event.events.common.LightningEvent;
 import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
 import dev.architectury.registry.fuel.FuelRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.GrassColor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
 
 public class JustNeeded {
     public static final String MOD_ID = "justneeded";
@@ -61,6 +70,15 @@ public class JustNeeded {
         FuelRegistry.register(90, Items.LARGE_FERN);
         FuelRegistry.register(90, Items.TALL_GRASS);
         FuelRegistry.register(90, Items.VINE);
+
+        LightningEvent.STRIKE.register((LightningBolt bolt, Level level, Vec3 pos, List<Entity> toStrike) -> {
+            BlockPos blockPos = new BlockPos((int) pos.x, (int) pos.y - 1, (int) pos.z);
+            if (level.getBlockState(blockPos).getBlock() == Blocks.SAND) {
+                level.setBlock(blockPos, Blocks.GLASS.defaultBlockState(), 3);
+            } else if (level.getBlockState(blockPos).getBlock() == Blocks.RED_SAND) {
+                level.setBlock(blockPos, Blocks.RED_STAINED_GLASS.defaultBlockState(), 3);
+            }
+        });
     }
 
     // TODO: Add blocks/items to tags
