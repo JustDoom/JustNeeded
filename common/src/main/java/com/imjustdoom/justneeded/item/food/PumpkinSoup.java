0c771538535d321188986f6c085e5cb3d34101d3
JustDoom <61824552+JustDoom@users.noreply.github.com>
2024-02-11 09:30:52 +1000

    Use vanilla bowl item class for pumpkin soup

--- common/src/main/java/com/imjustdoom/justneeded/item/food/PumpkinSoup.java
+++ common/src/main/java/com/imjustdoom/justneeded/item/food/PumpkinSoup.java
@@ -3,12 +3,13 @@ package com.imjustdoom.justneeded.item.food;
 import com.imjustdoom.justneeded.JustNeeded;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BowlFoodItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 
-public class PumpkinSoup extends Item {
+public class PumpkinSoup extends BowlFoodItem {
 
     public PumpkinSoup() {
         super(new Item.Properties()
@@ -19,10 +20,5 @@ public class PumpkinSoup extends Item {
                         .saturationMod(0.6f)
                         .build())
         );
-    }
-
-    public ItemStack finishUsingItem(ItemStack stack, Level worldIn, LivingEntity entityLiving) {
-        super.finishUsingItem(stack, worldIn, entityLiving);
-        return new ItemStack(Items.BOWL);
     }
 }
